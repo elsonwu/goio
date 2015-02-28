da5ff1ece34c0f7324f173600753211fbf46311e
Elson Wu <elsonwu@126.com>
2015-02-28 18:18:54 +0800

    use goji to replace the web framework

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,19 +14,15 @@ import (
 	"time"
 
 	"github.com/elsonwu/goio"
-	"github.com/go-martini/martini"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+	"github.com/zenazn/goji/web/middleware"
 )
 
-var flagHost = flag.String("host", "127.0.0.1:9999", "the default server host")
-var flagSSLHost = flag.String("sslhost", "", "the server host for https, it will override the host setting")
 var flagAllowOrigin = flag.String("alloworigin", "", "the host allow to cross site ajax")
 var flagDebug = flag.Bool("debug", false, "enable debug mode or not")
 var flagClientLifeCycle = flag.Int64("lifecycle", 30, "how many seconds of the client life cycle")
 var flagClientMessageTimeout = flag.Int64("messagetimeout", 15, "how many seconds of the client keep waiting for new messages")
-var flagEnableHttps = flag.Bool("enablehttps", false, "enable https or not")
-var flagDisableHttp = flag.Bool("disablehttp", false, "disable http and use https only")
-var flagCertFile = flag.String("certfile", "", "certificate file path")
-var flagKeyFile = flag.String("keyfile", "", "private file path")
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -47,27 +44,28 @@ func main() {
 		}()
 	}
 
-	martini.Env = martini.Dev
-	router := martini.NewRouter()
-	mart := martini.New()
-	mart.Action(router.Handle)
-	m := &martini.ClassicMartini{mart, router}
-	m.Use(martini.Recovery())
-	m.Use(func(res http.ResponseWriter) {
-		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		res.Header().Set("Access-Control-Allow-Credentials", "true")
-		res.Header().Set("Access-Control-Allow-Methods", "GET,POST")
-		if "" != *flagAllowOrigin {
-			allowOrigins := strings.Split(*flagAllowOrigin, ",")
-			for _, allowOrigin := range allowOrigins {
-				res.Header().Add("Access-Control-Allow-Origin", allowOrigin)
+	goji.Abandon(middleware.RequestID)
+	goji.Abandon(middleware.Logger)
+	goji.Use(func(h http.Handler) http.Handler {
+		fn := func(res http.ResponseWriter, r *http.Request) {
+			res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			res.Header().Set("Access-Control-Allow-Credentials", "true")
+			res.Header().Set("Access-Control-Allow-Methods", "GET,POST")
+			if "" != *flagAllowOrigin {
+				allowOrigins := strings.Split(*flagAllowOrigin, ",")
+				for _, allowOrigin := range allowOrigins {
+					res.Header().Add("Access-Control-Allow-Origin", allowOrigin)
+				}
 			}
+			res.Header().Set("Content-Type", "text/plain")
+			h.ServeHTTP(res, r)
 		}
+
+		return http.HandlerFunc(fn)
 	})
 
 	if *flagDebug {
-		m.Get("/test", func() string {
-
+		goji.Get("/test", func(w http.ResponseWriter, req *http.Request) {
 			st := time.Now().Unix()
 			for i := 0; i < 10000; i++ {
 				userId := strconv.Itoa(i)
@@ -84,11 +82,11 @@ func main() {
 				room.Add(user)
 			}
 
-			return strconv.Itoa(int(time.Now().Unix()-st)) + " seconds"
+			io.WriteString(w, strconv.Itoa(int(time.Now().Unix()-st))+" seconds")
 		})
 	}
 
-	m.Get("/count", func(req *http.Request) string {
+	goji.Get("/count", func(w http.ResponseWriter, req *http.Request) {
 		res := ""
 		res += fmt.Sprintf("rooms: %d, users: %d, clients: %d \n", rooms.Count(), users.Count(), clients.Count())
 
@@ -114,56 +112,80 @@ func main() {
 			})
 		}
 
-		return res
+		io.WriteString(w, res)
 	})
 
-	m.Get("/room/users/:room_id", func(params martini.Params, req *http.Request) (int, string) {
-		roomId := params["room_id"]
+	goji.Get("/room/users/:room_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		roomId := ctx.URLParams["room_id"]
 		if roomId == "" {
-			return 403, "room_id is missing"
+			http.Error(w, "room_id is missing", http.StatusForbidden)
+			return
 		}
 
 		room := rooms.Get(roomId, false)
 		if room == nil {
-			return 200, ""
+			io.WriteString(w, "room does not exist")
+			return
 		}
 
-		return 200, strings.Join(room.UserIds.Array(), ",")
+		io.WriteString(w, strings.Join(room.UserIds.Array(), ","))
 	})
 
-	m.Get("/user/data/:user_id/:key", func(params martini.Params, req *http.Request) (int, string) {
-		userId := params["user_id"]
-		if userId == "" {
-			return 403, "user_id is missing"
+	goji.Get("/room/users/:room_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		roomId := ctx.URLParams["room_id"]
+		if roomId == "" {
+			http.Error(w, "room_id is missing", http.StatusForbidden)
+			return
 		}
 
-		key := params["key"]
+		room := rooms.Get(roomId, false)
+		if room == nil {
+			io.WriteString(w, "")
+			return
+		}
+
+		io.WriteString(w, strings.Join(room.UserIds.Array(), ","))
+	})
+
+	goji.Get("/user/data/:user_id/:key", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		userId := ctx.URLParams["user_id"]
+		if userId == "" {
+			http.Error(w, "user_id is missing", http.StatusForbidden)
+			return
+		}
+
+		key := ctx.URLParams["key"]
 		if key == "" {
-			return 403, "key is missing"
+			http.Error(w, "key is missing", http.StatusForbidden)
+			return
 		}
 
 		user := users.Get(userId)
 		if user == nil {
-			return 200, ""
+			io.WriteString(w, "")
+			return
 		}
 
-		return 200, user.Data().Get(key)
+		io.WriteString(w, user.Data().Get(key))
 	})
 
-	m.Post("/user/data/:client_id/:key", func(params martini.Params, req *http.Request) (int, string) {
+	goji.Post("/user/data/:client_id/:key", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
 		val, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			return 500, err.Error()
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
 		}
 
-		clientId := params["client_id"]
+		clientId := ctx.URLParams["client_id"]
 		if clientId == "" {
-			return 403, "client_id is missing"
+			http.Error(w, "client_id is missing", http.StatusForbidden)
+			return
 		}
 
-		key := params["key"]
+		key := ctx.URLParams["key"]
 		if key == "" {
-			return 403, "key is missing"
+			http.Error(w, "key is missing", http.StatusForbidden)
+			return
 		}
 
 		clt := clients.Get(clientId)
@@ -174,13 +196,14 @@ func main() {
 			}
 		}
 
-		return 200, ""
+		io.WriteString(w, "")
 	})
 
-	m.Post("/online_status", func(params martini.Params, req *http.Request) (int, string) {
+	goji.Post("/online_status", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
 		val, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			return 500, err.Error()
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
 		}
 
 		userIds := strings.Split(string(val), ",")
@@ -193,11 +216,11 @@ func main() {
 			}
 		}
 
-		return 200, status
+		io.WriteString(w, status)
 	})
 
-	m.Post("/client/:user_id", func(params martini.Params, req *http.Request) (int, string) {
-		userId := params["user_id"]
+	goji.Post("/client/:user_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		userId := ctx.URLParams["user_id"]
 		user := users.Get(userId)
 		if user == nil {
 			user = goio.NewUser(userId)
@@ -207,24 +230,25 @@ func main() {
 		user.Add(clt)
 		done <- true
 
-		return 200, clt.Id
+		io.WriteString(w, clt.Id)
 	})
 
-	m.Get("/kill_client/:client_id", func(params martini.Params, req *http.Request) (int, string) {
-		id := params["client_id"]
+	goji.Get("/kill_client/:client_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		id := ctx.URLParams["client_id"]
 		clt := clients.Get(id)
 		if clt != nil {
 			clt.Destroy()
 		}
 
-		return 204, ""
+		io.WriteString(w, "")
 	})
 
-	m.Get("/message/:client_id", func(params martini.Params, req *http.Request) (int, string) {
-		id := params["client_id"]
+	goji.Get("/message/:client_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		id := ctx.URLParams["client_id"]
 		clt := clients.Get(id)
 		if clt == nil {
-			return 404, fmt.Sprintf("Client %s does not exist\n", id)
+			http.Error(w, fmt.Sprintf("Client %s does not exist\n", id), http.StatusNotFound)
+			return
 		}
 
 		clt.Handshake()
@@ -236,11 +260,13 @@ func main() {
 			if 0 < len(clt.Messages) {
 				msgs, err := json.Marshal(clt.Messages)
 				if err != nil {
-					return 500, err.Error()
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
 				}
 
 				clt.CleanMessages()
-				return 200, string(msgs)
+				io.WriteString(w, string(msgs))
+				return
 			}
 
 			if time.Now().Unix() > timeNow+*flagClientMessageTimeout {
@@ -250,19 +276,21 @@ func main() {
 			time.Sleep(100000 * time.Microsecond)
 		}
 
-		return 204, ""
+		io.WriteString(w, "")
 	})
 
-	m.Post("/message/:client_id", func(params martini.Params, req *http.Request) (int, string) {
-		id := params["client_id"]
+	goji.Post("/message/:client_id", func(ctx web.C, w http.ResponseWriter, req *http.Request) {
+		id := ctx.URLParams["client_id"]
 		clt := clients.Get(id)
 		if clt == nil {
-			return 403, fmt.Sprintf("Client %s does not exist\n", id)
+			http.Error(w, fmt.Sprintf("Client %s does not exist\n", id), http.StatusForbidden)
+			return
 		}
 
 		user := users.Get(clt.UserId)
 		if user == nil {
-			return 403, fmt.Sprintf("Client %s does not connect with any user\n", id)
+			http.Error(w, fmt.Sprintf("Client %s does not connect with any user\n", id), http.StatusForbidden)
+			return
 		}
 
 		clt.Handshake()
@@ -293,40 +321,8 @@ func main() {
 			user.Emit(message.EventName, message)
 		}(message)
 
-		return 204, ""
+		io.WriteString(w, "")
 	})
 
-	m.Options("/.*", func(req *http.Request) {})
-
-	host := *flagHost
-	if !*flagEnableHttps && *flagDisableHttp {
-		log.Fatalln("You cannot disable http but not enable https in the same time")
-	}
-
-	//Prevent exiting
-	ch := make(chan bool)
-
-	if !*flagDisableHttp {
-		go func() {
-			log.Println("Serve at " + host + " - http")
-			if err := http.ListenAndServe(host, m); err != nil {
-				log.Println(err)
-			}
-		}()
-	}
-
-	if *flagEnableHttps {
-		go func() {
-			if *flagSSLHost != "" {
-				host = *flagSSLHost
-			}
-
-			log.Println("Serve at " + host + " - https")
-			if err := http.ListenAndServeTLS(host, *flagCertFile, *flagKeyFile, m); err != nil {
-				log.Println(err)
-			}
-		}()
-	}
-
-	<-ch
+	goji.Serve()
 }
